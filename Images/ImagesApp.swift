ceff5ef8a22a0515c0c261cf695c5cb9d69a38a2
Razan <razanalzannan@gmail.com>
2023-05-29 23:14:36 +0300

    Initial Commit

---
+++ Images/ImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesApp.swift
+//  Images
+//
+//  Created by Razan Mohammed Alzannan on 09/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
